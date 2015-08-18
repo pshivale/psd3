83a7a3802f435943bb75ba90ae6e55d07f06c321
Pramod Shivale <pshivale@gmail.com>
2015-08-18 23:20:37 +0530

    graph tests , html reporter not working

--- karma.conf.js
+++ karma.conf.js
@@ -35,7 +35,23 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'html'],
+
+    // the default configuration
+    htmlReporter: {
+      outputDir: 'karma_html', // where to put the reports 
+      templatePath: null, // set if you moved jasmine_template.html
+      focusOnFailures: true, // reports show failures on start
+      namedFiles: false, // name files instead of creating sub-directories
+      pageTitle: null, // page title for reports; browser info by default
+      urlFriendlyName: false, // simply replaces spaces with _ for files/dirs
+      reportName: 'report-summary-filename', // report summary filename; browser info by default
+
+
+      // experimental
+      preserveDescribeNesting: false, // folded suites stay folded 
+      foldAll: false, // reports start folded (only with preserveDescribeNesting)
+    },
 
 
     // web server port
@@ -62,6 +78,13 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: false,
+/*
+    plugins: [
+        "karma-chrome-launcher",
+        "karma-firefox-launcher",
+        "karma-jasmine",
+        "karma-html-reporter"
+    ]*/
   })
 }
--- spec/graphSpec.js
+++ spec/graphSpec.js
@@ -4,8 +4,27 @@ describe("A suite", function() {
   });
 });
 
-describe("Graph suite", function() {
-  it("Graph Costructor", function() {
+describe("Graph", function() {
+  it("costructor works", function() {
     expect(new psd3.Graph({})).not.toBe(null);
   });
+
+  it("defaults are set", function() {
+  	var graph = new psd3.Graph({});
+    expect(graph.config.width).toBe(400);
+    expect(graph.config.height).toBe(400);
+    expect(graph.config.value).toBe("value");
+    expect(graph.config.inner).toBe("inner");
+    expect(graph.config.transition).toBe("linear");
+    expect(graph.config.transitionDuration).toBe(1000);
+    expect(graph.config.donutRadius).toBe(0);
+    expect(graph.config.gradient).toBe(false);
+    expect(graph.config.labelColor).toBe("black");
+    expect(graph.config.drilldownTransition).toBe("linear");
+    expect(graph.config.drilldownTransitionDuration).toBe(0);
+    expect(graph.config.stroke).toBe("white");
+    expect(graph.config.strokeWidth).toBe(2);
+    expect(graph.config.highlightColor).toBe("orange");
+  });
 });
+
